c6505631ae45b98e54502071701d0da87c5c2fa0
wahyubagus <wahyu.sulaksono@ist.id>
2022-10-26 00:54:19 +0700

    initial commit

---
+++ src/main/java/ars/cicd/Application.java
@@ -0,0 +1,20 @@
+package ars.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping(value = "/message")
+	public String message(){
+		return "the message";
+	}
+}
